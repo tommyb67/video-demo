45cb2b5ae6e82ebcf1e1f18ab25420d6d0152ced
Tom Brennan <tommybrennan@gmail.com>
2014-09-19 01:00:25 -0400

    set us the video model and the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 VideoDemo::Application.routes.draw do
+
+  resources :videos, only: [:index, :new, :create]
+  root to: 'videos#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
