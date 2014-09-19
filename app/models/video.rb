8968868e3f3f10d9c71697482e63038f1c5dbbf8
Tom Brennan <tommybrennan@gmail.com>
2014-09-19 01:09:25 -0400

    added code to fetch videos from youtube in the video model

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,21 @@
 class Video < ActiveRecord::Base
   params.require(:video).permit(:link)
+
+  YT_LINK_FORMAT = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/i
+  validates :link, presence: true, format: YT_LINK_FORMAT
+
+  before_create -> do
+      uid = link.match(YT_LINK_FORMAT)
+    self.uid = uid[2] if uid && uid[2]
+
+    if self.uid.to_s.length != 11
+      self.errors.add(:link, 'is invalid.')
+      false
+    elsif Video.where(uid: self.uid).any?
+      self.errors.add(:link, 'is not unique.')
+      false
+    else
+      get_additional_info
+    end
+  end
 end
